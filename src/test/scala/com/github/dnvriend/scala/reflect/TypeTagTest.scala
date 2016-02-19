508f019e9dae606e02c88410d8e2c184ba6b7098
Dennis Vriend <dnvriend@gmail.com>
2016-02-19 21:18:53 +0100

    classTag

--- src/test/scala/com/github/dnvriend/scala/reflect/TypeTagTest.scala
+++ src/test/scala/com/github/dnvriend/scala/reflect/TypeTagTest.scala
@@ -19,13 +19,25 @@ package com.github.dnvriend.scala.reflect
 import com.github.dnvriend.TestSpec
 
 import scala.reflect.runtime.universe._
+import scala.reflect.{ClassTag, classTag}
 
+/**
+  * Should be included in Scala 2.12M6
+  * see: SI-6517
+  */
 class TypeTagTest extends TestSpec {
 
   class Foo
   class Bar extends Foo
 
-  def determineTypeImplicit[A](xs: List[A])(implicit ev: TypeTag[A]) = typeOf[A] match {
+  def determineTypeImplicitClassTag[A : ClassTag](xs: List[A]) = classTag[A] match {
+    case ct if ct == classTag[String] => "strings"
+    case ct if ct == classTag[Int] => "ints"
+    case ct if ct == classTag[Bar] => "bars"
+    case ct if ct == classTag[Foo] => "foos"
+  }
+  
+  def determineTypeImplicitTypeTag[A](xs: List[A])(implicit ev: TypeTag[A]) = typeOf[A] match {
     case t if t =:= typeOf[String] ⇒ "strings" //  =:= (type equality)
     case t if t =:= typeOf[Int]    ⇒ "ints"
     case t if t <:< typeOf[Bar] => "bars" //  <:< (subtype relation)
@@ -42,8 +54,8 @@ class TypeTagTest extends TestSpec {
   // non-variable type argument String in type pattern List[String] (the underlying of List[String]) is unchecked
   // since it is eliminated by erasure
   def determineTypeNaive[A](xs: List[A]) = xs match {
-    case _: List[String] ⇒ "strings"
-    case _: List[Int]    ⇒ "ints"
+    case _: List[String @unchecked] ⇒ "strings"
+    case _: List[Int @unchecked]    ⇒ "ints"
   }
 
   "determineTypeNaive" should "match ints" in {
@@ -70,19 +82,35 @@ class TypeTagTest extends TestSpec {
     determineType(List(new Foo)) shouldBe "foos"
   }
 
-  "determineTypeImplicit" should "match ints" in {
-    determineTypeImplicit(List(1, 2)) shouldBe "ints"
+  "determineTypeImplicitTypeTag" should "match ints" in {
+    determineTypeImplicitTypeTag(List(1, 2)) shouldBe "ints"
   }
 
   it should "match strings" in {
-    determineTypeImplicit(List("a", "b")) shouldBe "strings"
+    determineTypeImplicitTypeTag(List("a", "b")) shouldBe "strings"
   }
 
   it should "match bars" in {
-    determineTypeImplicit(List(new Bar)) shouldBe "bars"
+    determineTypeImplicitTypeTag(List(new Bar)) shouldBe "bars"
   }
 
   it should "match foo" in {
-    determineTypeImplicit(List(new Foo)) shouldBe "foos"
+    determineTypeImplicitTypeTag(List(new Foo)) shouldBe "foos"
+  }
+
+  "determineTypeImplicitClassTag" should "match ints" in {
+    determineTypeImplicitClassTag(List(1, 2)) shouldBe "ints"
+  }
+
+  it should "match strings" in {
+    determineTypeImplicitClassTag(List("a", "b")) shouldBe "strings"
+  }
+
+  it should "match bars" in {
+    determineTypeImplicitClassTag(List(new Bar)) shouldBe "bars"
+  }
+
+  it should "match foo" in {
+    determineTypeImplicitClassTag(List(new Foo)) shouldBe "foos"
   }
 }
